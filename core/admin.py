b19750d823426caca79614f6ae904ae053d42872
Aan Kurniawan <aan.linuxer@gmail.com>
2014-06-12 14:53:39 +0700

    Userauth module

---
+++ core/admin.py
@@ -0,0 +1,20 @@
+from django.contrib import admin
+
+
+class RontalAdmin(admin.ModelAdmin):
+	actions = ['soft_delete']
+
+	def soft_delete(self, request, queryset):
+		for object in queryset:
+			object.delete()
+	
+	soft_delete.short_description = "Delete selected items"
+
+	def get_actions(self, request):
+		actions = super(RontalAdmin, self).get_actions(request)
+		del actions['delete_selected']
+		return actions
+		
+	def queryset(self, request):
+		query = super(RontalAdmin, self).queryset(request)
+		return query.filter(deleted_at__isnull=True)
---
+++ userauth/admin.py
@@ -0,0 +1,35 @@
+from userauth.models import Group, UserStatus, User, UserPassword, UserHistory
+from django.contrib import admin
+from core.admin import RontalAdmin
+
+
+class GroupAdmin(RontalAdmin):
+	fields = ['name', 'description']
+	list_display = ('name', 'is_protected', 'description')
+
+
+class UserStatusAdmin(RontalAdmin):
+	fields = ['status', 'is_protected', 'description']
+	list_display = ('status', 'is_protected', 'description')
+
+
+class UserAdmin(RontalAdmin):
+	fields = ['alias', 'is_protected', 'email', 'group', 'user_status']
+	list_display = ('alias', 'is_protected', 'email', 'group', 'user_status')
+
+
+class UserPasswordAdmin(RontalAdmin):
+	fields = ['user', 'password', 'is_protected']
+	list_display = ('user', 'is_protected', 'password')
+
+
+class UserHistoryAdmin(RontalAdmin):
+	fields = ['user', 'user_log']
+	list_display = ('user', 'user_log', 'created_at')
+
+
+admin.site.register(Group, GroupAdmin)
+admin.site.register(UserStatus, UserStatusAdmin)
+admin.site.register(User, UserAdmin)
+admin.site.register(UserPassword, UserPasswordAdmin)
+admin.site.register(UserHistory, UserHistoryAdmin)
